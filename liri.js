92c9e9e9a38766ea349cdcdc8df7822ba7715718
cmac <chelsey.macneill@gmail.com>
2019-06-04 11:37:36 -0700

    Fixed error messaging for Bands in town API call

--- liri.js
+++ liri.js
@@ -1,37 +1,32 @@
 //++++++++++++++++++ Set up +++++++++++++++++++++++++++++++
 // Installed 
 // Spotify : node-spotify-api, [Node-Spotify-API](https://www.npmjs.com/package/node-spotify-api)
+let spotifyPackage = require('node-spotify-api');
 // Axios : [Axios](https://www.npmjs.com/package/axios)
+let axios = require("axios")
 // Moment: (https://www.npmjs.com/package/monpmment)
+let moment = require("moment")
 // DotEnv: (https://www.npmjs.com/package/dotenv)
+require("dotenv").config();
 // FS : 
+let fs = require("fs");
 
 // No Install needed:
 // OMDB API : (http://www.omdbapi.com)
 // Bands In Town API : (http://www.artists.bandsintown.com/bandsintown-api)
 
-// Axios 
-let axios = require("axios")
-// Core node package for reading and writing files
-let fs = require("fs");
-
-
-// Spotify API Set up ===============================
-// Read and set any environment variables with the dotenv package
-require("dotenv").config();
 
 //import the `keys.js` file and store it in a variable.
 let keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api');
 
-// Retrieve data via Axios from Spotify
-let spotify = new Spotify(keys.spotify);
+// Access the Spotify keys
+let spotify = new spotifyPackage(keys.spotify);
 
 //console.log(JSON.stringify(keysOfSpotify))
 //console.log(keysOfSpotify.id)
 //console.log(keysOfSpotify.secret)
-// ===================================================
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // Gathering input from the CLI 
 let command = process.argv[2];
@@ -62,7 +57,7 @@ switch (command) {
 // Spotify API call 
 function spotifyAPI() {
   // Takes input from the CLI for the Spotify API call and returns 1 result
-  spotify.search({ type: 'track', query: 'input', limit:1 }, function(err, data) {
+  spotify.search({ type: 'track', query: userInput, limit:1 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
@@ -79,7 +74,7 @@ function spotifyAPI() {
     console.log(result)
     
     // Writing the results of Spotify API Call to log.txt 
-    fs.writeFile("log.txt", result , function(err) {
+    fs.appendFile("log.txt", result , function(err) {
       
       // If the code experiences any errors it will log the error to the console.
       if (err) {
@@ -96,6 +91,8 @@ function spotifyAPI() {
 function movie() {
   axios.get("http://www.omdbapi.com/?t=" + userInput + "&apikey=trilogy").then(
   function(response) {
+
+    console.log(response)
     
     let result = "\n-----------------------------------" +
     "\nTitle: " + JSON.stringify(response.data.Title) +
@@ -147,48 +144,64 @@ function movie() {
 // Bands in Town API CALL 
 function concert() {
   axios.get("https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp").then(
-  function(response) {
-    console.log("")
-    console.log("SEE BELOW FOR UPCOMING CONCERTS. BAND: " + userInput)
-    console.log("-------------------------------------")
-    console.log(response.data[0].venue.name);
-    console.log(response.data[0].venue.city + " " + response.data[0].venue.region);
-    console.log(moment(response.data[0].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY'));
-    console.log("-------------------------------------")
-    console.log(response.data[1].venue.name);
-    console.log(response.data[1].venue.city + " " + response.data[1].venue.region);
-    console.log(moment(response.data[1].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY'));
-    console.log("-------------------------------------")
-    console.log(response.data[2].venue.name);
-    console.log(response.data[2].venue.city + " " + response.data[2].venue.region);
-    console.log(moment(response.data[2].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY'));
-    console.log("-------------------------------------")
-    console.log(response.data[3].venue.name);
-    console.log(response.data[3].venue.city + " " + response.data[3].venue.region);
-    console.log(moment(response.data[3].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY'));
-    console.log("-------------------------------------")
-  })
-  .catch(function(error) {
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.log("---------------Data---------------");
-      console.log(error.response.data);
-      console.log("---------------Status---------------");
-      console.log(error.response.status);
-      console.log("---------------Status---------------");
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      // `error.request` is an object that comes back with details pertaining to the error that occurred.
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log("Error", error.message);
-    }
-    console.log(error.config);
-  });
-};
+    function(response) {
+     
+      // For trouble shooting error messages
+      // console.log(response)
+      // console.log(response.data[1].venue.name)
+      // console.log(response.data[1].venue.city)
+      // console.log(response.data[1].venue.region)
+     
+
+      // Holds the response in a variable to be used for console logging and writing to a log file
+      concertResponse =
+        ( "\n" + "-------------------------------------" + "\n" +
+          response.data[0].venue.name + "\n" + response.data[0].venue.city + " " + response.data[0].venue.region + "\n" + moment(response.data[0].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY')
+          + "\n" + "-------------------------------------" + "\n" +
+          response.data[1].venue.name + "\n" + response.data[1].venue.city + " " + response.data[1].venue.region + "\n" + moment(response.data[0].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY')
+          + "\n" + "-------------------------------------" + "\n" +
+          response.data[2].venue.name + "\n" + response.data[2].venue.city + " " + response.data[2].venue.region + "\n" + moment(response.data[0].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY')
+          + "\n" + "-------------------------------------" + "\n" +
+          response.data[3].venue.name + "\n" + response.data[3].venue.city + " " + response.data[3].venue.region + "\n" + moment(response.data[0].datetime.substring(0,10), 'YYYY MM DD').format('MM/DD/YY')
+          + "\n" + "-------------------------------------"
+          );
+ 
+      console.log( userInput + " is playing at the following times and locations: " + concertResponse);
+ 
+      fs.appendFile("log.txt", concertResponse, function(err) {
+ 
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+          return console.log(err);
+        }
+ 
+        // Otherwise, it will print: "movies.txt was updated!"
+        console.log("");
+      });
+ 
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log("---------------Data---------------");
+        console.log(error.response.data);
+        console.log("---------------Status---------------");
+        console.log(error.response.status);
+        console.log("---------------Status---------------");
+        console.log(error.response.headers);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log("Error Request: " + error.request);
+      } else if (error.message) {
+        // Something happened in setting up the request that triggered an Error
+        
+        console.log("Sorry this artist has no upcoming events.");
+      }
+      console.log(error.config);
+    });
+ }
 
 // Do what it says functionality
 function doWhatItSays() {
